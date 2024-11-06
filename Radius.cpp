9300e6fdf63add690adc0ba56b56e52d6894a308
Kim Villagen.kuezmik <kuezmik@gmail.com>
2024-11-06 10:17:21 +0800

    Add files via upload

---
+++ Radius.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // constant declaration; 
+  const double Pi = 3.14159;
+  // variable declaration;
+  double radius;
+  double area;
+  
+  // assignment statement}
+  cout << "Enter circle radius"
+  area = * radius * radius
+  cout << "Area : " << area << endl;
+  return 0;
+}
+  
